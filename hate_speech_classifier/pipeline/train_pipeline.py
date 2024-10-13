4329a477b58ce0de039bbed83594db47086a5c44
Saurabh Satasia <saurabhsatasia@icloud.com>
2024-10-13 12:44:31 +0200

    added model trainer component

--- hate_speech_classifier/pipeline/train_pipeline.py
+++ hate_speech_classifier/pipeline/train_pipeline.py
@@ -3,17 +3,22 @@ from hate_speech_classifier.logger import logging
 from hate_speech_classifier.exception import CustomException
 from hate_speech_classifier.components.data_ingestion import DataIngestion
 from hate_speech_classifier.components.data_transformation import DataTransformation
+from hate_speech_classifier.components.model_trainer import ModelTrainer
 from hate_speech_classifier.entity.config_entity import (DataIngestionConfig,
-                                                         DataTransformationConfig)
+                                                         DataTransformationConfig,
+                                                         ModelTrainerConfig,
+                                                         )
 
 from hate_speech_classifier.entity.artifact_entity import (DataIngestionArtifacts,
-                                                           DataTransformationArtifacts,)
+                                                           DataTransformationArtifacts,
+                                                           ModelTrainerArtifacts,)
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_transformation_config = DataTransformationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
     def start_data_ingestion(self) -> DataIngestionArtifacts:
         logging.info("Entered the start_data_ingestion method of TrainPipeline class")
@@ -45,12 +50,32 @@ class TrainPipeline:
         except Exception as e:
             raise CustomException(e, sys) from e
 
+    def start_model_trainer(self, data_transformation_artifacts: DataTransformationArtifacts) -> ModelTrainerArtifacts:
+        logging.info(
+            "Entered the start_model_trainer method of TrainPipeline class"
+        )
+        try:
+            model_trainer = ModelTrainer(data_transformation_artifacts=data_transformation_artifacts,
+                                        model_trainer_config=self.model_trainer_config
+                                        )
+            model_trainer_artifacts = model_trainer.initiate_model_trainer()
+            logging.info("Exited the start_model_trainer method of TrainPipeline class")
+            return model_trainer_artifacts
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
     def run_pipeline(self):
         logging.info("Entered the run_pipeline method of TrainPipeline class")
         try:
             data_ingestion_artifacts = self.start_data_ingestion()
+
             data_transformation_artifacts = self.start_data_transformation(
                 data_ingestion_artifacts=data_ingestion_artifacts
+            )
+
+            model_trainer_artifacts = self.start_model_trainer(
+                data_transformation_artifacts=data_transformation_artifacts
             )
 
             logging.info("Exited the run_pipeline method of TrainPipeline class")
